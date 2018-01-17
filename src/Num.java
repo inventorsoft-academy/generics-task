d65f60f80d28f968ca38b81b74ca00f1889695f5
Andrew Orobets <veborocom@gmail.com>
2018-01-17 11:18:14 +0200

    Generics task

---
+++ src/Num.java
@@ -0,0 +1,52 @@
+//class-container for numbers
+
+import java.util.Collections;
+import java.util.List;
+
+public class Num <T extends Number> {
+
+    private List<T> collectin;
+
+    public Num(List<T> collectin) {
+        this.collectin = collectin;
+    }
+
+    public List<T> getCollectin() {
+        return collectin;
+    }
+
+    //utility method, which counts even numbers in given collection;
+    public String evenNumber(List<T> coll) {
+        double even = 0;
+        int cout = 0;
+        for (T c : coll) {
+            even = Double.valueOf(c.toString());
+            if (even % 2 == 0) {
+                cout++;
+            }
+        }
+        return "In the List we have " + cout + " numbers";
+
+    }
+
+    //method which takes two lists and index and replaces element
+    // at given position in the first collection with corresponding
+    // element from the second one, without possibility to do vice versa
+
+    public void replaceList(List<T> list1, List<T> list2, int index){
+        list1.set(index, list2.get(index));
+    }
+
+    // utility method, which takes two lists and index and switches elements at given position;
+    public void switchList(List<T> list1, List<T> list2, int index){
+        T tmp = list1.get(index);
+        list1.set(index, list2.get(index));
+        list2.set(index, tmp);
+    }
+
+    //utility method, which finds maximum element in the given list.
+    public Comparable maximumElement(List list){
+        return Collections.max(list);
+    }
+}
+
---
+++ src/Test.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+
+        Num<Integer> num = new Num<Integer>(list);
+        System.out.println(num.evenNumber(list));
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list2.add(-1);
+        list2.add(-2);
+        list2.add(-3);
+
+        //replaces element
+        num.replaceList(list1,list2,0);
+        System.out.println(list1);
+
+        //switches elemet
+        num.switchList(list1,list2,1);
+        System.out.println(list1+"\n"+list2);
+
+        //maximum element
+        System.out.println(num.maximumElement(list1));
+    }
+}
